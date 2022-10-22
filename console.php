23d51f0fd97f38f0304cecce2d59edb7d78c065d
aljazpockaj <apockaj@gmail.com>
2022-10-22 08:58:30 +0200

    Api called and decoded to array. Returns name of dogs.

--- console.php
+++ console.php
@@ -1,3 +1,10 @@
 <?php
 echo "Hello world";
+$api_url = "https://api.thedogapi.com/v1/breeds";
+$data = file_get_contents($api_url);
+$data = json_decode($data);
+foreach ($data as $dog) {
+    var_dump($dog->name);
+ }
+
 ?>
