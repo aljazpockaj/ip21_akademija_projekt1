4cc35dea0366f9f6482fc1cacc5155492e60e771
aljazpockaj <apockaj@gmail.com>
2022-10-25 21:14:52 +0200

    Added search query for dogs

--- console.php
+++ console.php
@@ -1,10 +1,25 @@
 <?php
-echo "Hello world";
+var_dump($argc);
+var_dump($argv);
 $api_url = "https://api.thedogapi.com/v1/breeds";
 $data = file_get_contents($api_url);
 $data = json_decode($data);
-foreach ($data as $dog) {
-    var_dump($dog->name);
- }
+$name = $argv[1];
+if(empty($name))
+{
+    foreach ($data as $dog) {
+        print_r($dog->name."\n");
+     }
+}
+else if($name){
+    $datalength = count($data);
+    for($i = 0; $i<$datalength;$i++){
+        if(str_contains($data[$i]->name,$name)){
+            print_r($data[$i]->name."\n");
+        }
+    }
+}
+
+
 
 ?>
