cb93715518a26babd2ff0fe8d44edf7fa53829a4
aljazpockaj <apockaj@gmail.com>
2022-10-31 12:47:51 +0100

    Limited string on search query.

--- console.php
+++ console.php
@@ -1,24 +1,36 @@
 <?php
 if(isset($argv[1])){
     $name = $argv[1];
+}else{
+    return 0;
 }
-if(empty($name))
+if(is_string($name) and ctype_alpha($name))
 {
-    $api_url = "https://api.thedogapi.com/v1/breeds";
-    $data = file_get_contents($api_url);
-    $data = json_decode($data);
-    foreach ($data as $dog) {
-       echo $dog->name."\n";
-     }
+    if(strlen($name) > 20){
+        echo "Ime je predolgo. Maksimalna dolžina je 20 znakov.";
+    }
+    else{
+        if(empty($name))
+        {
+            $api_url = "https://api.thedogapi.com/v1/breeds";
+            $data = file_get_contents($api_url);
+            $data = json_decode($data);
+            foreach ($data as $dog) {
+               echo $dog->name."\n";
+             }
+        }
+        else if($name){
+            $api_url = "https://api.thedogapi.com/v1/breeds/search?q=".$name;
+            $data = file_get_contents($api_url);
+            $data = json_decode($data);
+            $datalength = count($data);
+            foreach ($data as $dog) {
+                echo $dog->name."\n";
+              }
+        }
+    }
 }
-else if($name){
-    $api_url = "https://api.thedogapi.com/v1/breeds/search?q=".$name;
-    $data = file_get_contents($api_url);
-    $data = json_decode($data);
-    $datalength = count($data);
-    foreach ($data as $dog) {
-        echo $dog->name."\n";
-      }
+else{
+    echo "Vnesti je potrebno pravilno ime pasme.";
 }
-
 ?>
