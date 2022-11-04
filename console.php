da287dee7e395fbdd98ea4213b29e6993b0a416f
aljazpockaj <apockaj@gmail.com>
2022-11-04 14:56:31 +0100

    Added two functions for api calls.

--- console.php
+++ console.php
@@ -1,36 +1,48 @@
 <?php
+function parameterApiCall($name){
+    $api_url = "https://api.thedogapi.com/v1/breeds/search?q=".$name;
+    $data = file_get_contents($api_url);
+    $data = json_decode($data);
+    return $data;
+}
+function emptyApiCall(){
+    $api_url = "https://api.thedogapi.com/v1/breeds";
+    $data = file_get_contents($api_url);
+    $data = json_decode($data);
+    return $data;
+}
+
 if(isset($argv[1])){
     $name = $argv[1];
-}else{
-    return 0;
-}
-if(is_string($name) and ctype_alpha($name))
+    if(is_string($name) and ctype_alpha($name))
 {
     if(strlen($name) > 20){
         echo "Ime je predolgo. Maksimalna dolžina je 20 znakov.";
     }
     else{
-        if(empty($name))
-        {
-            $api_url = "https://api.thedogapi.com/v1/breeds";
-            $data = file_get_contents($api_url);
-            $data = json_decode($data);
-            foreach ($data as $dog) {
-               echo $dog->name."\n";
-             }
-        }
-        else if($name){
-            $api_url = "https://api.thedogapi.com/v1/breeds/search?q=".$name;
-            $data = file_get_contents($api_url);
-            $data = json_decode($data);
-            $datalength = count($data);
+            if($name){
+            $data = parameterApiCall($name);
             foreach ($data as $dog) {
                 echo $dog->name."\n";
               }
+            if(empty($dog)){
+                echo "Ni rezultatov vašega iskanja.";
+            }
         }
     }
 }
 else{
     echo "Vnesti je potrebno pravilno ime pasme.";
 }
+}
+else{
+    if(empty($name))
+    {
+        $data = emptyApiCall();
+        foreach ($data as $dog) {
+           echo $dog->name."\n";
+         }
+    }
+}
+
 ?>
