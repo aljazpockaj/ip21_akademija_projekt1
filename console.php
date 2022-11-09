3f85fb418ef9b8fdb93cb7a76941b0c804247d5d
aljazpockaj <apockaj@gmail.com>
2022-11-09 14:18:51 +0100

    Used ternary logic for function.

--- console.php
+++ console.php
@@ -1,51 +1,31 @@
 <?php
-if(isset($argv[1])){
+if (isset($argv[1])) {
     $name = $argv[1];
-    if(is_string($name) and ctype_alpha($name))
-{
-    if(strlen($name) > 20){
-        echo "Ime je predolgo. Maksimalna dolžina je 20 znakov.";
-    }
-    else{
-            if($name){
+    if (is_string($name) and ctype_alpha($name)) {
+        if (strlen($name) > 20) {
+            echo "Ime je predolgo. Maksimalna dolžina je 20 znakov.";
+        } else {
             $data = apiCall($name);
             foreach ($data as $dog) {
-                echo $dog->name."\n";
-              }
-            if(empty($dog)){
-                echo "Ni rezultatov vašega iskanja.";
+                echo $dog->name . "\n";
+                if (empty($dog)) {
+                    echo "Ni rezultatov vašega iskanja.";
+                }
             }
         }
+    } else {
+        echo "Vnesti je potrebno pravilno ime pasme.";
+    }
+} else {
+    $data = apiCall($name = "");
+    foreach ($data as $dog) {
+        echo $dog->name . "\n";
     }
 }
-else{
-    echo "Vnesti je potrebno pravilno ime pasme.";
+function apiCall($name)
+{
+    empty($name) ? $api_url = "https://api.thedogapi.com/v1/breeds" : $api_url = "https://api.thedogapi.com/v1/breeds/search?q=" . $name;
+    $data = file_get_contents($api_url);
+    $data = json_decode($data);
+    return $data;
 }
-}
-else{
-    if(empty($name))
-    {
-        $name = "";
-        $data = apiCall($name);
-        foreach ($data as $dog) {
-           echo $dog->name."\n";
-         }
-    }
-}
-
-function apiCall($name){
-    if($name==""){
-        $api_url = "https://api.thedogapi.com/v1/breeds";
-        $data = file_get_contents($api_url);
-        $data = json_decode($data);
-        return $data;           
-    }
-    else{
-        $api_url = "https://api.thedogapi.com/v1/breeds/search?q=".$name;
-        $data = file_get_contents($api_url);
-        $data = json_decode($data);
-        return $data;
-    }
-}
-
-?>
