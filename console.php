6f1222fc80700010000610176e5ed2b827666db8
aljazpockaj <apockaj@gmail.com>
2022-11-30 15:22:16 +0100

    Fixed getAnimal function, added two more functions

--- console.php
+++ console.php
@@ -10,7 +10,6 @@ function apiCall($name = "", $type)
     } else if ($type == "cats") {
         $api_url = empty($name) ? "https://api.thecatapi.com/v1/breeds" : "https://api.thecatapi.com/v1/breeds/search?q=" . $name;
     }
-
     $content = file_get_contents($api_url);
     $data = $content == false ? throw new Exception("Error urlja") : json_decode($content);
     if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
@@ -27,23 +26,31 @@ function printAnimal($data)
 function getAnimal($function, $type, $name)
 {
     if (isset($function) and isset($type)) {
-        if ($function == "search" and isset($name)) {
-            if (isset($name) and is_string($name) and ctype_alpha($name) and strlen($name) <= 20) {
-                $data = apiCall($name, $type);
-                if (empty($data)) {
-                    echo "Ni rezultatov vašega iskanja.";
-                } else {
-                    printAnimal($data);
-                }
-            } else {
-                echo "Vnesti je potrebno pravilno ime pasme.";
-            }
-        } else if ($function == "list" and $type == "cats" or $function == "list" and $type == "dogs") {
-            $type = ($type == "dogs") ? "dogs" : "cats";
-            printAnimal(apiCall($name, $type));
+        checkFunction($function, $type, $name);
+    } else {
+        echo "Nisi vnesel pravilnih parametrov.";
+    }
+}
+function checkName($name, $type)
+{
+    if (isset($name) and is_string($name) and ctype_alpha($name) and strlen($name) <= 20) {
+        $data = apiCall($name, $type);
+        if (empty($data)) {
+            echo "Ni rezultatov vašega iskanja.";
         } else {
-            echo "Nisi vnesel pravilnih parametrov.";
+            printAnimal($data);
         }
+    } else {
+        echo "Vnesti je potrebno pravilno ime pasme.";
+    }
+}
+function checkFunction($function, $type, $name)
+{
+    if ($function == "search" and isset($name)) {
+        checkName($name, $type);
+    } else if ($function == "list" and $type == "cats" or $function == "list" and $type == "dogs") {
+        $type = ($type == "dogs") ? "dogs" : "cats";
+        printAnimal(apiCall($name, $type));
     } else {
         echo "Nisi vnesel pravilnih parametrov.";
     }
