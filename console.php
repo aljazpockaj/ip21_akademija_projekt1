a20678828bd7684bf74f5d9fb51fd56713b98c93
aljazpockaj <apockaj@gmail.com>
2023-02-14 21:37:00 +0100

    Added optional parameter -v that outputs temperament for each animal

--- console.php
+++ console.php
@@ -2,6 +2,14 @@
 $function = $argv[1] ?? null;
 $type = $argv[2] ?? null;
 $name = $argv[3] ?? null;
+$option = end($argv) ?? null;
+$num_args = count($argv);
+foreach ($argv as $i => $arg) {
+    if ($name == "-v") {
+        $name = null;
+        $option = '-v';
+    }
+}
 require_once("lib/model.php");
 require_once("lib/views/consoleView.php");
 require_once 'vendor/autoload.php';
@@ -16,14 +24,21 @@ $twig = new Environment($loader, [
     'debug' => true
 ]);
 $twig->addExtension(new \Twig\Extension\DebugExtension());
-$template = $twig->load('test.twig');
 try {
-    $animals = $model->getAnimals($function, $type, $name);
-    echo $template->render(['test.twig', 'animals' => $animals]);
+    if ($argv[$num_args - 1] == '-v') {
+        $template = $twig->load('test2.twig');
+        $animals = $model->getAnimals($function, $type, $name);
+        echo $template->render(['test2.twig', 'animals' => $animals]);
+    } else {
+        $template = $twig->load('test.twig');
+        $animals = $model->getAnimals($function, $type, $name);
+        echo $template->render(['test.twig', 'animals' => $animals]);
+    }
     #$view->showAnimals($model->getAnimals($function, $type, $name));
 } catch (Exception $e) {
     echo "Caught exception: " . $e->getMessage();
 }
+
 function animalType($type)
 {
     if ($type == "dogs" or $type == "cats" or $type == "both") {
@@ -35,6 +50,9 @@ function animalType($type)
 }
 function nameValid($name)
 {
+    if ($name == "-v") {
+        return;
+    }
     if ($name == null) {
         echo "Nisi vnesel imena.";
         return;
